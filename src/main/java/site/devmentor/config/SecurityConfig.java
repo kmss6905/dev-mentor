7b0ec29ba84c060f045c038fab36a32a89e8ebb3
kmss6905 <kmss69052@naver.com>
2023-09-01 14:40:35 +0900

    feat: #4 로그인 기능 구현
     * Spring Security Starter 의존성 추가
     * JsonLoginAuthentication 인증 필터 추가
    
     참고
     #2 PasswordServicer -> PasswordEncoder 로 대체

---
+++ src/main/java/site/devmentor/config/SecurityConfig.java
@@ -0,0 +1,96 @@
+package site.devmentor.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.util.AntPathMatcher;
+import site.devmentor.auth.CustomUserDetailService;
+import site.devmentor.auth.filter.JsonLoginAuthenticationFilter;
+import site.devmentor.auth.filter.JsonLoginAuthenticationSuccessHandler;
+import site.devmentor.domain.user.UserRepository;
+
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+  private final ObjectMapper objectMapper;
+  private final UserRepository userRepository;
+
+  public SecurityConfig(ObjectMapper objectMapper, UserRepository userRepository) {
+    this.objectMapper = objectMapper;
+    this.userRepository = userRepository;
+  }
+
+  @Bean
+  public AbstractAuthenticationProcessingFilter jsonLoginAuthenticationFilter() {
+    JsonLoginAuthenticationFilter jsonLoginAuthenticationFilter = new JsonLoginAuthenticationFilter(objectMapper);
+    jsonLoginAuthenticationFilter.setAuthenticationManager(authenticationManager());
+    jsonLoginAuthenticationFilter.setAuthenticationSuccessHandler(jsonLoginSuccessHandler());
+    return jsonLoginAuthenticationFilter;
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    // disable default formLogin
+    http
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .logout(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable);
+
+    http
+            .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                    authorizationManagerRequestMatcherRegistry
+                            .requestMatchers(
+                                    antMatcher(POST, "/login"),
+                                    antMatcher(POST, "/user"),
+                                    antMatcher(GET, "/user/*/exists"),
+                                    antMatcher(GET, "/user/email/*/exists")
+                            ).permitAll()
+                            .anyRequest().authenticated());
+
+    // add json login filter
+    http
+            .addFilterBefore(jsonLoginAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    return http.build();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager() {
+    DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+    daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+    daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+    return new ProviderManager(daoAuthenticationProvider);
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    return new CustomUserDetailService(userRepository);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationSuccessHandler jsonLoginSuccessHandler() {
+    return new JsonLoginAuthenticationSuccessHandler(objectMapper);
+  }
+}
--- src/main/java/site/devmentor/domain/user/UserRepository.java
+++ src/main/java/site/devmentor/domain/user/UserRepository.java
@@ -3,10 +3,14 @@ package site.devmentor.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
   boolean existsUserByUserId(String userId);
 
   boolean existsUserByEmail(String email);
+
+  Optional<User> findUserByEmail(String email);
 }
