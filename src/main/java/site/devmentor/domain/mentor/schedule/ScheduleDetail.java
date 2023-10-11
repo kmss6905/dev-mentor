c58a0a2681fe638cf274b2f197b229ca517feac1
kmss6905 <kmss69052@naver.com>
2023-10-11 17:24:12 +0900

    feat: #40 세부 스케줄 삭제
    1. 세부 스케줄 삭제
    2. 세부 스케줄 인수 테스트 추가

--- src/main/java/site/devmentor/domain/mentor/schedule/ScheduleDetail.java
+++ src/main/java/site/devmentor/domain/mentor/schedule/ScheduleDetail.java
@@ -7,6 +7,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.Where;
 import site.devmentor.auth.AppUser;
 import site.devmentor.domain.BaseEntity;
 import site.devmentor.domain.mentor.schedule.vo.*;
@@ -17,86 +18,95 @@ import java.time.LocalDateTime;
 @Table(name = "MENTORING_SCHEDULE_DETAIL")
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Where(clause = "is_deleted=false")
 @DynamicUpdate
 public class ScheduleDetail extends BaseEntity {
 
-  @Size(max = 255)
-  @NotNull
-  @Column(name = "title", nullable = false)
-  private String title;
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "title", nullable = false)
+    private String title;
 
-  @Enumerated
-  private ScheduleDetailTime time;
+    @Enumerated
+    private ScheduleDetailTime time;
 
-  @Enumerated
-  private ScheduleDetailMemo memo;
+    @Enumerated
+    private ScheduleDetailMemo memo;
 
-  @Column(name = "status", nullable = false)
-  @Enumerated(value = EnumType.STRING)
-  private ScheduleDetailStatus status = ScheduleDetailStatus.PENDING;
+    @Column(name = "status", nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private ScheduleDetailStatus status = ScheduleDetailStatus.PENDING;
 
-  @Column(name = "deleted_at")
-  private LocalDateTime deletedAt;
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
 
-  @Column(name = "is_deleted")
-  private boolean isDeleted = false;
+    @Column(name = "is_deleted")
+    private boolean isDeleted = false;
 
-  @ManyToOne
-  @JoinColumn(name = "schedule_id")
-  private Schedule schedule;
+    @ManyToOne
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
 
-  @Builder
-  private ScheduleDetail(final String title, final ScheduleDetailMemo memo, final ScheduleDetailTime time, final Schedule schedule) {
-    verifyMemoNotNull(memo);
-    verifyTitleNotNull(title);
-    verifyTimeNotNull(time);
-    this.memo = memo;
-    this.title = title;
-    this.time = time;
-    this.schedule = schedule;
-  }
-
-  private void verifyTimeNotNull(ScheduleDetailTime time) {
-    if (time == null) {
-      throw new ScheduleDetailContentValidateException("time can't not be null");
+    @Builder
+    private ScheduleDetail(final String title, final ScheduleDetailMemo memo, final ScheduleDetailTime time, final Schedule schedule) {
+        verifyMemoNotNull(memo);
+        verifyTitleNotNull(title);
+        verifyTimeNotNull(time);
+        this.memo = memo;
+        this.title = title;
+        this.time = time;
+        this.schedule = schedule;
     }
-  }
 
-  private void verifyMemoNotNull(ScheduleDetailMemo memo) {
-    if (memo == null) {
-      throw new ScheduleDetailContentValidateException("memo can't not be null");
+    private void verifyTimeNotNull(ScheduleDetailTime time) {
+        if (time == null) {
+            throw new ScheduleDetailContentValidateException("time can't not be null");
+        }
     }
-  }
 
-  private void verifyTitleNotNull(String title) {
-    if (title == null || title.trim().isEmpty()) {
-      throw new ScheduleDetailContentValidateException("title can't not be empty");
+    private void verifyMemoNotNull(ScheduleDetailMemo memo) {
+        if (memo == null) {
+            throw new ScheduleDetailContentValidateException("memo can't not be null");
+        }
     }
-  }
 
-  public void update(ScheduleDetail detail) {
-    this.memo = detail.memo;
-    this.time = detail.time;
-    this.title = detail.title;
-  }
+    private void verifyTitleNotNull(String title) {
+        if (title == null || title.trim().isEmpty()) {
+            throw new ScheduleDetailContentValidateException("title can't not be empty");
+        }
+    }
 
-   public boolean isScheduleAuthor(AppUser appUser) {
-     return this.schedule.isAuthorOf(appUser);
-  }
+    public void update(ScheduleDetail detail) {
+        this.memo = detail.memo;
+        this.time = detail.time;
+        this.title = detail.title;
+    }
 
-  public String getTitle() {
-    return title;
-  }
+    public void delete() {
+        if (isDeleted) {
+            throw new IllegalStateException("detail is already deleted");
+        }
+        this.deletedAt = LocalDateTime.now();
+        this.isDeleted = true;
+    }
 
-  public ScheduleDetailTime getTime() {
-    return time;
-  }
+    public boolean isScheduleAuthor(AppUser appUser) {
+        return this.schedule.isAuthorOf(appUser);
+    }
 
-  public ScheduleDetailMemo getMemo() {
-    return memo;
-  }
+    public String getTitle() {
+        return title;
+    }
 
-  public ScheduleDetailStatus getStatus() {
-    return status;
-  }
+    public ScheduleDetailTime getTime() {
+        return time;
+    }
+
+    public ScheduleDetailMemo getMemo() {
+        return memo;
+    }
+
+    public ScheduleDetailStatus getStatus() {
+        return status;
+    }
 }
---
+++ src/main/java/site/devmentor/dto/mentor/schedule/ScheduleDetailUpdateResponse.java
@@ -0,0 +1,15 @@
+package site.devmentor.dto.mentor.schedule;
+
+import site.devmentor.domain.mentor.schedule.ScheduleDetail;
+
+import java.time.LocalDateTime;
+
+public record ScheduleDetailUpdateResponse(
+        long detailId,
+        LocalDateTime updatedAt
+) {
+
+  public static ScheduleDetailUpdateResponse of(ScheduleDetail detail) {
+    return new ScheduleDetailUpdateResponse(detail.getId(), detail.getUpdatedAt());
+  }
+}
